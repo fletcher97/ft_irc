4915b7e896f495fbd40cafb4f8013a8ec2ad7eef
Miguel Gueifao <miguel.gueifao@gmail.com>
2023-02-05 00:35:47 +0100

    Changed log output from cout to clog

--- src/log/Log.cpp
+++ src/log/Log.cpp
@@ -5,36 +5,36 @@
 
 void
 Log::trace(const std::stringstream& msg) {
-	std::cout << TRACE_COLOR_FG << "[TRACE]" << RESET_COLOR << ": " <<
+	std::clog << TRACE_COLOR_FG << "[TRACE]" << RESET_COLOR << ": " <<
 		msg.str() << RESET_COLOR << std::endl;
 }
 
 void
 Log::debug(const std::stringstream& msg) {
-	std::cout << DEBUG_COLOR_FG << "[DEBUG]" << RESET_COLOR << ": " <<
+	std::clog << DEBUG_COLOR_FG << "[DEBUG]" << RESET_COLOR << ": " <<
 		msg.str()  << RESET_COLOR << std::endl;
 }
 
 void
 Log::info(const std::stringstream& msg) {
-	std::cout << INFO_COLOR_FG << "[INFO]" << RESET_COLOR << " : " <<
+	std::clog << INFO_COLOR_FG << "[INFO]" << RESET_COLOR << " : " <<
 		msg.str()  << RESET_COLOR << std::endl;
 }
 
 void
 Log::warn(const std::stringstream& msg) {
-	std::cout << WARN_COLOR_FG << "[WARNS]" << RESET_COLOR << ": " <<
+	std::clog << WARN_COLOR_FG << "[WARNS]" << RESET_COLOR << ": " <<
 		msg.str()  << RESET_COLOR << std::endl;
 }
 
 void
 Log::error(const std::stringstream& msg) {
-	std::cout << ERROR_COLOR_FG << "[ERROR]" << RESET_COLOR << ": " <<
+	std::clog << ERROR_COLOR_FG << "[ERROR]" << RESET_COLOR << ": " <<
 		msg.str()  << RESET_COLOR << std::endl;
 }
 
 void
 Log::fatal(const std::stringstream& msg) {
-	std::cout << FATAL_COLOR_FG << "[FATAL]" << RESET_COLOR << ": " <<
+	std::clog << FATAL_COLOR_FG << "[FATAL]" << RESET_COLOR << ": " <<
 		msg.str()  << RESET_COLOR << std::endl;
 }
