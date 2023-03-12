1b0e2d4f0e4ae831802793e88422308d5e8d4045
sebamiro <seba.mi.21@gmail.com>
2023-03-12 16:33:41 +0100

    Class base constructor and methods

--- inc/Server.hpp
+++ inc/Server.hpp
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <map>
+#include <vector>
 
 #include "Client.hpp"
 #include "Channel.hpp"
@@ -10,14 +11,31 @@
 class Server
 {
 private:
-	std::string _name;
-	std::map<std::string, Client&> _clients;
+	int _fd;
+	std::map<int, Client&> _clients;
 	std::map<std::string, Channel&> _channels;
 public:
-	Server(std::string name);
+	Server();
 	Server(const Server& s);
 	Server& operator=(const Server& s);
 	~Server();
+	
+	void	init();
+	void	run();
+	
+	Client&	getClient(const std::string& nickname);
+	Channel&	getChannel(const std::string& name);
+	
+	std::vector<Client*>	getClients();
+	std::vector<Channel*>	getChannels();
+	
+	void	delClient(Client& client);
+	void	delChannel(Channel& channel);
+	
+	bool	isChannel(const std::string& name) const;
+private:
+	typedef std::map<int, Client&>::iterator clients_iterator;
+	typedef std::map<std::string, Channel&>::iterator channels_iterator;
 };
 
 #endif // SERVER_HPP
--- src/Channel.cpp
+++ src/Channel.cpp
@@ -1,23 +1,71 @@
 #include "Channel.hpp"
 #include "Log.hpp"
 
-Channel::Channel(std::string name) {
+Channel::Channel() :	_name(),
+											_topic(),
+											_clients(),
+											_key()
+{
 	LOG_DEBUG("Creating new channel");
-	this->_name = name;
 	LOG_INFO("New channel created: " << name);
 }
 
-Channel::Channel(const Channel& c) {
-	this->_name = c._name;
-	this->_clients = c._clients;
-}
+Channel::Channel(const Channel& c) :	_name(c._name),
+																			_topic(c._topic),
+																			_clients(c._clients),
+																			_key(c._key)
+{}
 
 Channel &Channel::operator=(const Channel& c) {
-	this->_name = c._name;
+	this->_name = c.getName();
+	this->_topic = c.getTopic();
 	this->_clients = c._clients;
+	this->_key = c.getKey();
 	return *this;
 }
 
 Channel::~Channel() {
 	LOG_INFO("Removed channel: " << this->name);
+}
+
+std::string
+Channel::getName() const {
+	return this->_name;
+}
+
+std::string
+Channel::getTopic() const {
+	return this->_topic;
+}
+
+std::string
+Channel::getKey() const {
+	return this->_key;
+}
+
+// std::vector<Client*>
+// Channel::getClients() const {
+	
+// }
+
+void
+Channel::setName(std::string name) {
+	this->_name = name;
+}
+
+void
+Channel::setTopic(std::string topic) {
+	this->_topic = topic;
+}
+
+void
+Channel::setKey(std::string key) {
+	this->_key = key;
+}
+
+bool
+Channel::isInChannel(const Client& client) {
+	if (this->_clients.count(client.getFd()) == 1)
+		return true;
+	return false;
 }
