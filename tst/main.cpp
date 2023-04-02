186f6eca48cb309efec3cc3e943e7d1069ae40bd
Miguel Gueifao <miguel.gueifao@gmail.com>
2023-04-02 14:18:19 +0200

    Added ParserUT to tst main

--- tst/main.cpp
+++ tst/main.cpp
@@ -3,12 +3,14 @@
 #include "ChannelUT.hpp"
 #include "ServerUT.hpp"
 #include "ClientUT.hpp"
+#include "ParserUT.hpp"
 
 int
 main() {
 	ADD_TEST(ChannelUT)
 	ADD_TEST(ClientUT)
 	ADD_TEST(ServerUT)
+	ADD_TEST(ParserUT)
 
 	flt::TestCollection::instance().test();
 	flt::TestCollection::instance().report(std::cout);
