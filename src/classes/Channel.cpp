288c41ee8a117370af39e997816ade129f95cdf0
Sebastian Miro <seba.mi.21@gmail.com>
2023-04-14 11:29:02 +0200

    Added logs to Channel

--- src/classes/Channel.cpp
+++ src/classes/Channel.cpp
@@ -66,11 +66,13 @@ ft_irc::Channel::getKey(void) const {
 void
 ft_irc::Channel::setName(const std::string& name) {
 	if (name.length() == 0)
+		LOG_WARN("setName called with empty string")
 		throw std::invalid_argument("Name must be a non empty string");
 	if ((name.at(0) != '#' && name.at(0) != '&')
 		|| name.find(' ') != std::string::npos
 		|| name.find(0x07) != std::string::npos
 		|| name.find(',') != std::string::npos)
+		LOG_WARN("setName called with an invalid character")
 		throw ft_irc::Channel::InvalidChannelName("Invalid channel  name: " + name);
 	LOG_INFO("Channel's name changed from: " << this->_name << " to: " << name)
 	this->_name = name;
@@ -79,11 +81,13 @@ ft_irc::Channel::setName(const std::string& name) {
 void
 ft_irc::Channel::setTopic(ft_irc::Client& source, std::string& topic) {
 	if (!this->_clients.count(source.getFd()))
+		LOG_WARN("setTopic called with a client not on channel")
 		throw ft_irc::Channel::NotOnChannel();
 	if (this->_mode & PROTECTED_TOPIC
 		&& !(this->_clients.at(source.getFd()).mode & OPERATOR)
 		&& !(this->_clients.at(source.getFd()).mode & HALFOP)
 		&& !(this->_clients.at(source.getFd()).mode & PROTECTED))
+		LOG_WARN("setTopic called with a client with no privileges")
 		throw ft_irc::Channel::NoPrivsOnChannel();
 	LOG_INFO("Channel's topic changed from: " << this->_topic << " to: " << topic)
 	this->_topic = topic;
@@ -92,6 +96,7 @@ ft_irc::Channel::setTopic(ft_irc::Client& source, std::string& topic) {
 void
 ft_irc::Channel::setKey(std::string& key) {
 	if (key.length() == 0)
+		LOG_WARN("setKet with an empty string")
 		throw std::invalid_argument("Key must be a non empty string");
 	LOG_INFO("Channel's key changed from: " << this->_key << " to: " << key)
 	this->_key = key;
@@ -100,6 +105,7 @@ ft_irc::Channel::setKey(std::string& key) {
 void
 ft_irc::Channel::setClientLimit(long limit) {
 	if (limit <= 0)
+		LOG_WARN("setClientLimit with an invalid number: " << limit)
 		throw ft_irc::Channel::InvalidLimit("Invalid limit");
 	LOG_INFO("Channel's limit changed from: " << this->_client_limit << " to: " << limit)
 	this->_client_limit = limit;
@@ -109,6 +115,7 @@ void
 ft_irc::Channel::toggleMode(const char mode) {
 	if (mode > (INVITE|MODERATE|SECRET|PROTECTED_TOPIC|NOT_EXTERNAL_MSGS)
 		|| mode <= 0)
+		LOG_WARN("setClientLimit with an invalid mode: " << mode)
 		throw ft_irc::Channel::InvalidMode("Invalid mode");
 	this->_mode ^= mode;
 }
@@ -122,19 +129,23 @@ bool
 ft_irc::Channel::isInChannel(const std::string& nickname) {
 	for (client_iterator it = this->_clients.begin(); it != this->_clients.end(); it++)
 		if (it->second.client.getNickname() == nickname)
+			LOG_INFO("isInChannel client in channel: " << nickname)
 			return true;
+	LOG_INFO("isInChannel client not in channel: " << nickname)
 	return false;
 }
 
 bool
 ft_irc::Channel::addClient(const ft_irc::Client& client) {
 	if (this->_clients.count(client.getFd()))
+		LOG_WARN("addClient client already on channel: " << client.getNickname())
 		return false;
 	this->_clients.insert(std::make_pair<int, ft_irc::Channel::ClientInfo>(client.getFd(),
 		ft_irc::Channel::ClientInfo(client)));
-	if (this->_clients.size() == 1)
-		this->_clients.begin()->second.mode = (FOUNDER|OPERATOR);
 	LOG_INFO("New client added to channel: " << client.getNickname())
+	if (this->_clients.size() == 1)
+		LOG_INFO("addClient first client in channel: " << client.getNickname())
+		this->_clients.begin()->second.mode = (FOUNDER|OPERATOR);
 	return true;
 }
 
@@ -142,6 +153,7 @@ bool
 ft_irc::Channel::banMask(const std::string& mask) {
 	if (this->_masks.count(mask)) {
 		if (this->_masks[mask] & BAN)
+			LOG_WARN("banMask mask already banned in channel: " << mask)
 			return false;
 		this->_masks[mask] ^= BAN;
 	}
@@ -154,34 +166,43 @@ ft_irc::Channel::banMask(const std::string& mask) {
 bool
 ft_irc::Channel::invite(const Client& source, const std::string& nick) {
 	if (!this->_clients.count(source.getFd()))
+		LOG_WARN("invite, source not on channel: " << source.getNickname())
 		throw ft_irc::Channel::NotOnChannel();
 	if (this->_mode & INVITE_ONLY && !(this->_clients.at(source.getFd()).mode & OPERATOR))
+		LOG_WARN("invite, source doesent have privileges: " << source.getNickname())
 		throw ft_irc::Channel::NoPrivsOnChannel();
 	if (this->isInChannel(nick))
+		LOG_WARN("invite, client already on channel: " << nick)
 		throw ft_irc::Channel::AlreadyOnChannel();
 	if (this->_masks.count(nick)) {
 		if (this->_masks[nick] & INVITE)
+			LOG_WARN("invite, client already invited to channel: " << nick)
 			return false;
 		this->_masks[nick] ^= INVITE;
 	}
 	else
 		this->_masks.insert(std::make_pair<std::string, mask_mode>(nick, INVITE));
-	LOG_INFO("Client invited to channel: " << nick)
+	LOG_INFO("invite, client invited to channel: " << nick)
 	return true;
 }
 
 bool
 ft_irc::Channel::join(const ft_irc::Client& client, const std::string& key) {
 	if (this->_clients.count(client.getFd()))
+		LOG_WARN("join, client already in channel: " << client.getNickname())
 		return false;
 	if (this->_masks.count(client.getMask()) && this->_masks[client.getMask()] & BAN)
+		LOG_WARN("join, client banned from channel: " << client.getNickname())
 		throw ft_irc::Channel::BannedClient();
 	if (this->_mode & INVITE_ONLY &&
 		(!this->_masks.count(client.getMask()) || !(this->_masks[client.getMask()] & INVITE)))
+		LOG_WARN("join, client not invited to channel: " << client.getNickname())
 		throw ft_irc::Channel::InviteOnlyChannel();
 	if (this->_client_limit != 0 && this->_client_limit >= this->_clients.size())
+		LOG_WARN("join, client can't join, channel is full: " << client.getNickname())
 		throw ft_irc::Channel::ChannelIsFull();
 	if (!this->_key.empty() && this->_key != key)
+		LOG_WARN("join, client " << client.getNickname() << " can't join, invalid key: " << key)
 		throw ft_irc::Channel::InvalidKey("Incorrect channel key");
 	LOG_INFO("Client joined to channel: " << client.getNickname())
 	this->addClient(client);
